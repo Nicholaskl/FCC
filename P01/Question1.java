0219d25913e5ca7157912bc75b1db71eff590e5d
NicholasKl <19782944@student.curtin.edu.au>
2020-03-09 17:58:19 +0800

    Fixed Question 1 - prac 1

--- P01/Question1.java
+++ P01/Question1.java
@@ -2,33 +2,67 @@ import java.util.*;
 
 public class Question1
 {
+   private static final int C_VALUE = 3;
+
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         String plaintext = "";
+	String cipherText = "";
 
         System.out.println("Please enter text");
         plaintext = sc.next();
 
-        System.out.println(EncryptString(plaintext));
+	plaintext = plaintext.toLowerCase();	
 
-        System.out.println(plaintext);
+	cipherText = EncryptString(plaintext);
+        System.out.println("Encrypted: " + cipherText);
+	
+	plaintext = DecryptString(plaintext);
+	System.out.println("Decrypted: " + plaintext);
     }
 
     public static String EncryptString(String plaintext)
     {
-        int currInt = 0;
-        char currChar = 'a';
         String cipherText = "";
+	int[] temp = new int[plaintext.length()];
 
         for (int i=0; i < plaintext.length(); i++)
         {
-            currChar = plaintext.charAt(i);
-            currInt = Integer.parseInt((String)currChar);
-            currInt = (currInt + 3) % 26;
-            currChar = (char)String.valueOf(currInt);
-            ciphertext += currChar;
+            temp[i] = (int)(plaintext.charAt(i)) - 97;
+            temp[i] = ((temp[i] + C_VALUE) % 26) + 97;
         }
+
+	for (int i=0; i < temp.length; i++)
+	{
+		cipherText += (char)temp[i];
+	}
+
+        return cipherText;
+    }
+
+    public static String DecryptString(String plaintext)
+    {
+        String cipherText = "";
+	int[] temp = new int[plaintext.length()];
+
+        for (int i=0; i < plaintext.length(); i++)
+        {
+            temp[i] = (int)(plaintext.charAt(i)) - 97;
+	    if ((temp[i]-3) < 0)
+	    {
+		temp[i] = temp[i] + 120;
+	    }
+	    else
+	    {
+		temp[i] = ((temp[i] - C_VALUE) % 26) + 97;
+	    }
+        }
+
+	for (int i=0; i < temp.length; i++)
+	{
+		cipherText += (char)temp[i];
+	}
 
         return cipherText;
     }
